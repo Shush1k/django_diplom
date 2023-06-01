722506358485bb028d71d18dce8b85baabab7cef
Shush1k <Shush1k.b@gmail.com>
2023-06-01 13:02:18 +0300

    Update email_verify field in django admin

--- django_one/users/admin.py
+++ django_one/users/admin.py
@@ -1,10 +1,41 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-# Register your models here.
+
 User = get_user_model()
 
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
     pass
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ('username', 'password')
+            }
+        ),
+
+        (
+            'Персональная информация',
+            {
+                'fields': ('first_name', 'last_name', 'email')
+            }
+        ),
+        (
+            'Права доступа',
+            {
+                'fields':
+                    ('is_active', 'email_verify', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+            }
+         ),
+        (
+            'Важные даты',
+            {
+                'fields':
+                    ('last_login', 'date_joined')
+            }
+        )
+    ]
+    readonly_fields = ('last_login', 'date_joined',)
+    list_display = ('email', 'is_superuser', 'email_verify', 'date_joined',)
--- django_one/users/models.py
+++ django_one/users/models.py
@@ -6,7 +6,8 @@ from django.utils.translation import gettext_lazy as _
 class User(AbstractUser):
     email = models.EmailField(_('Email'), unique=True)
 
-    email_verify = models.BooleanField(default=False)
+    email_verify = models.BooleanField(_('Email верифицирован'), default=False,
+                                       help_text="Указывает, что пользователь подтвердил электронную почту.")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
